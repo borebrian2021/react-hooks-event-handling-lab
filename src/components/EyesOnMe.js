fcd3418dc2d5e0bea963c84a51e09e34a7710d18
borebrian2021 <bkimutai2021@gmail.com>
2022-07-14 20:57:54 +0300

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,19 @@
 // Code EyesOnMe Component Here
+import userEvent from '@testing-library/user-event';
+function EyesOnMe(){
+    return(<button onBlur={(e) => {
+        console.log('Hey! Eyes on me!')
+     }} 
+      onFocus={()=>{
+        console.log('Good!')
+      }} >Eyes on me</button>)
+}
+function focusArrow(){
+    ;
+
+    }
+    function blurArrow(){
+        console.log('Hey! Eyes on me!')
+    
+        }
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,11 @@
 // Code Keypad Component Here
+import userEvent from '@testing-library/user-event';
+function Keypad(){
+    return (
+        <input type="password" onChange={() => console.log("Entering password...")}/>
+    )
+}
+
+
+
+export default Keypad
